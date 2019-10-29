2432da8b7713b784f29b7064a9503d3bae77ac6a
laylatame <laylatame@hotmail.com>
2019-10-28 21:54:07 -0600

    First commit

---
+++ Clase/Begginer/agosto18.c
@@ -0,0 +1,42 @@
+// Layla Tame
+// A01192934
+// Agosto 18, 2016
+
+
+#include <stdio.h>
+
+int main()
+{
+// Programa 1
+
+    int a, b;
+    float c, data;
+
+    printf("Here we'll use some variables \n");
+
+    a = 6;
+    b = a/4;
+    c = a/4;
+    data = 45.2387;
+
+    printf("A contains %d \n", a);
+    printf("B contains %d \n", b);
+    printf("C contains %d \n", c);
+    printf("Data contains %d \n", data);
+
+    return 0;
+
+
+// Programa 2
+
+    float celcius, faren, converfact;
+
+    converfact = 9/5
+
+    printf("Enter a temperature in degrees Celcius:" \n);
+    scanf_s("%f", &celcius);
+
+    faren = (celcius * converfact) + 32;
+
+    printf("The temperature in farenheit is %f \n", faren);
+}
